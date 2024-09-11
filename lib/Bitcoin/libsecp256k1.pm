0d78f20608dd9c1e7a0c60d0de2cc133725c6a62
bbrtj <bbrtj.pro@gmail.com>
2024-09-11 12:13:01 +0200

    Initial - mint dist

---
+++ lib/Bitcoin/libsecp256k1.pm
@@ -0,0 +1,41 @@
+package Bitcoin::libsecp256k1;
+
+use v5.14;
+use warnings;
+
+require XSLoader;
+XSLoader::load('Bitcoin::libsecp256k1', $Bitcoin::libsecp256k1::VERSION);
+
+1;
+
+__END__
+
+=head1 NAME
+
+Bitcoin::libsecp256k1 - New module
+
+=head1 SYNOPSIS
+
+	use Bitcoin::libsecp256k1;
+
+	# do something
+
+=head1 DESCRIPTION
+
+This module lets you blah blah blah.
+
+=head1 SEE ALSO
+
+L<Some::Module>
+
+=head1 AUTHOR
+
+Bartosz Jarzyna E<lt>bbrtj.pro@gmail.comE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2024 by Bartosz Jarzyna
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
---
+++ libsecp256k1.xs
@@ -0,0 +1,9 @@
+#define PERL_NO_GET_CONTEXT
+#include "EXTERN.h"
+#include "perl.h"
+#include "XSUB.h"
+
+MODULE = Bitcoin::libsecp256k1				PACKAGE = Bitcoin::libsecp256k1
+
+PROTOTYPES: DISABLED
+

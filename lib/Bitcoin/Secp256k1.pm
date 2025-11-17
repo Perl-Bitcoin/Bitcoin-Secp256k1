0054252362cc7d535bbcccd80374e4600dc8af36
bbrtj <bbrtj.pro@gmail.com>
2025-11-17 18:01:23 +0100

    Add Reginaldo Costa to contributors

--- lib/Bitcoin/Secp256k1.pm
+++ lib/Bitcoin/Secp256k1.pm
@@ -685,6 +685,16 @@ L<Bitcoin::Crypto>
 
 Bartosz Jarzyna E<lt>bbrtj.pro@gmail.comE<gt>
 
+=head2 Contributors
+
+In no particular order:
+
+=over
+
+=item * Reginaldo Costa
+
+=back
+
 =head1 COPYRIGHT AND LICENSE
 
 Copyright (C) 2024 by Bartosz Jarzyna
